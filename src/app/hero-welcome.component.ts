e495deb454da2fb2129273c118cb20d65bf00f56
Martin <Martin@DESKTOP-R8J1KC7>
2017-07-11 19:44:35 +0200

    reorganized components

---
+++ src/app/hero-welcome.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'my-hero-welcome',
+  templateUrl: './hero-welcome.component.html'
+})
+export class HeroWelcomeComponent {
+
+}
